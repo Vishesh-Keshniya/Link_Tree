58ce4d3963b4b538ae9a747f44a0afe127d0f663
Vishesh keshniya <keshniyavisheshk20@gmail.com>
2025-02-22 12:55:18 +0530

    Fixed 404 refresh issue

--- LinkTree/vite.config.js
+++ LinkTree/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Change this from "/" to "./"
+  base: "./", // Change this from "/" to "./" for relative paths
+  build: {
+    outDir: "dist"
+  }
 });
