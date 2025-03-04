652a73ac7dc380a3354f6f430191441479a94d77
Vishesh-Keshniya <119745743+Vishesh-Keshniya@users.noreply.github.com>
2025-03-04 16:03:38 +0530

    Update vite.config.js

--- LinkTree/dist/vite.config.js
+++ LinkTree/dist/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Change this from "/" to "./" for relative paths
+  base: "/", // Change this from "/" to "./" for relative paths
   build: {
     outDir: "dist"
   }
