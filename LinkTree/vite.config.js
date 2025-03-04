04e1064149496623201121472a6599a2805aec32
Vishesh keshniya <keshniyavisheshk20@gmail.com>
2025-03-04 16:12:34 +0530

    sdf

--- LinkTree/vite.config.js
+++ LinkTree/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+  import { defineConfig } from "vite";
+  import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "./", // Change this from "/" to "./" for relative paths
-  build: {
-    outDir: "dist"
-  }
-});
+  export default defineConfig({
+    plugins: [react()],
+    base: "/", // Change this from "/" to "./" for relative paths
+    build: {
+      outDir: "dist"
+    }
+  });
